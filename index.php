ce561dbf01421f5e5ebf3152fab1d47bfc5c8122
codedgift <codedgift@gmail.com>
2018-04-14 05:52:32 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>HNG INTERSHIP Stage #1</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style type="text/css">
+            body{
+                background-image: url("image/img.jpg");
+                background-size: cover;
+                background-position: center center;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div id="header" style="margin-top: 1%; margin-left: 1%;">
+            <p style="color: #CDF1FC; font-family: poppins; font-weight: bold; font-size: 22px;">Hotelsng</p>
+        </div>
+        
+        <div id="main" style="text-align: center; margin-top: 10%;">
+            <h3 style="color: #DA083D; font-weight: bold; font-size: 48px; font-family: arial black;">Welcome To</h3>
+            <h1 style="font-weight: bold; font-size: 60px; margin-top: -3%; font-family: arial black; color: #fff;">HNG INTERNSHIP 4</h1>
+            <div id="info" style="background: #DA083D; width: 17%; padding-left: 1px; padding-right: 1px; margin: 0px auto;">
+                <p style="color: #fff; font-size: 45px; font-weight: bold; font-family: arial black;">
+                    <?php 
+                        date_default_timezone_set('Africa/Lagos');
+                        echo date("h:i A" );
+
+                        ?>
+                </p>
+            </div>
+            <div id="footer" style="margin-top: 15%;">
+                <p style="color: #CDF1FC; font-size: 18px; font-weight: bold; font-family: arial black;">Powered by Hotels.ng</p>
+            </div>
+        </div>
+
+    </body>
+</html>
